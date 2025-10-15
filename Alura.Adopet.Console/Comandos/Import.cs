4b15618cb81f9dcb55fe357f35a44aa2f8f51ec7
felipelundgren-bsatech <felipe.lundgren@bsatech.io>
2025-10-15 14:29:09 -0300

    Feat: adicionando biblioteca Moq

--- Alura.Adopet.Console/Comandos/Import.cs
+++ Alura.Adopet.Console/Comandos/Import.cs
@@ -8,6 +8,13 @@ namespace Alura.Adopet.Console.Comandos
         documentacao: "adopet import <ARQUIVO> comando que realiza a importação do arquivo de pets.")]
     internal class Import:IComando
     {
+        private readonly HttpClientPet clientPet;
+
+        public Import(HttpClientPet clientpet)
+        {
+            this.clientPet = clientpet;
+        }
+
         public async Task ExecutarAsync(string[] args)
         {
             await this.ImportacaoArquivoPetAsync(caminhoDoArquivoDeImportacao: args[1]);
@@ -22,8 +29,8 @@ namespace Alura.Adopet.Console.Comandos
                 System.Console.WriteLine(pet);
                 try
                 {
-                    var httpCreatePet = new HttpClientPet();
-                    await httpCreatePet.CreatePetAsync(pet);
+                   
+                    await clientPet.CreatePetAsync(pet);
                 }
                 catch (Exception ex)
                 {
